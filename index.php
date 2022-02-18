aa910e9fdb977eabf7a2ad14c630d6bd9bdddedf
ErnestDam <ernestdamjutglar@gmail.com>
2022-02-18 20:58:32 +0100

    prova

--- index.php
+++ index.php
@@ -1,6 +1,53 @@
 <?php include 'header.php'; ?>
 
-<h1>HOLA</h1>
+
+<section class="post-list">
+        <div class="content">
+            <article class="post">
+                <div class="post-header">
+                    <div class="post-img-1"></div>
+                </div>
+                <div class="post-body">
+                    <span>@Tykuzz</span>
+                    <h2>Nou Tesla CyberTruck</h2>
+                    <p class="descripcion">Cybertruck está fabricado con un chasis exterior hecho para aportar la mayor durabilidad y protección a los pasajeros. Comenzando por un exoesqueleto casi impenetrable, todos los componentes están diseñados para proporcionar resistencia y solidez superiores, desde el revestimiento de la estructura de acero inoxidable laminado en frío de 30x ultrarresistente, hasta el cristal blindado de Tesla.</p>
+                    <a href="#" class="post-link">Read More...</a>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post-header">
+                    <div class="post-img-2"></div>
+                </div>
+                <div class="post-body">
+                    <span>@JordiTio</span>
+                    <h2>Bugatti Chyron 300</h2>
+                    <p class="descripcion">El Bugatti Chiron es un hiperdeportivo de Bugatti, que reemplazó al Bugatti Veyron en 2016. El Bugatti Chiron se caracteriza por ser el vehículo más rápido del mundo, siendo capaz de sobrepasar los 490 kilómetros por hora.</p>
+                    <a href="#" class="post-link">Read More...</a>
+                </div>
+            </article>
+            <article class="post">
+                <div class="post-header">
+                    <div class="post-img-3"></div>
+                </div>
+                <div class="post-body">
+                    <span>@ErnestDam</span>
+                    <h2>Ford Mustang Mach-e</h2>
+                    <p class="descripcion">Tras varios meses de expectación después de que se publicase una cifra errónea y mucho más baja de lo esperado, el Ford Mustang Mach-E, el primer SUV eléctrico del fabricante norteamericano, por fin tiene precio oficial en España: se puede configurar en cuatro versiones, elegir entre dos baterías y tiene un precio de partida de 48.473 euros.</p>
+                    <a href="#" class="post-link">Read More...</a>
+                </div>
+            </article>
+        </div>
+    </section>
+
+    <script src="main.js"></script>
 
 </body>
+
+
+
+
+
+
+
+
 </html>

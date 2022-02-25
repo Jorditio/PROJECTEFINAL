cd64235e1962950476cb13e26a939ac9ba8dd064
ErnestDam <ernestdamjutglar@gmail.com>
2022-02-25 20:27:08 +0100

    25/02/2022

--- insert.php
+++ insert.php
@@ -1,6 +1,16 @@
 <?php include 'header.php'; ?>
 
-<h1>INSERT</h1>
+<?php
 
+
+echo '<form>
+        <input type="text" name="factures" placeholder="factures">
+        <input type="text" name="facturesC" placeholder="facturescorrectes">
+        <input type="text" name="facturesI" placeholder="facturesincorrecte">
+        <input type="submit" name="processar">
+        </form>';
+
+
+        ?>
 </body>
 </html>

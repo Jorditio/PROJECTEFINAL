f70f09c78ed8c5077494585e8b43c6223c456b22
Jorditio <81320747+Jorditio@users.noreply.github.com>
2022-04-22 21:53:18 +0200

    minim

--- cotxes.php
+++ cotxes.php
@@ -1,46 +1,37 @@
+<?php include 'connexio.php'; ?>
 <?php include 'header.php'; 
-           
+class CRUD extends Connexio
+{
+  public function select()
+  {
+    $stmt = Connexio::connectar()->prepare("SELECT * from pujades, usuaris where pujades.usuaris_idusuaris = usuaris.idusuaris order by time desc");
+    $stmt->execute();
+    return $stmt->fetchAll();
+  }
+}
+
+$cmd = new CRUD();
+$fotos = $cmd->select();
+echo '<section class="post-list">
+<div class="content">';
+foreach ($fotos as $fo) {
+  echo '<article class="post">
+      <div class="post-header">
+          <div class="post-img-1">
+            <img src="' . $fo["fotos"] . '">
+          </div>
+      </div>
+      <div class="post-body">
+          <span></span>
+          <h2>'.$fo["nom"].'</h2>
+          <p class="descripcion">'.$fo["descripcio"].' </p>
+          <p> '.$fo["time"].'</p>
+      </div>
+  </article>';
+}
+echo '</div>
+</section>';         
 ?>
-
-
-<section class="post-list">
-        <div class="content">
-            <article class="post">
-                <div class="post-header">
-                    <div class="post-img-1"></div>
-                </div>
-                <div class="post-body">
-                    <span></span>
-                    <h2>Nou Tesla CyberTruck</h2>
-                    <p class="descripcion">Cybertruck está fabricado con un chasis exterior hecho para aportar la mayor durabilidad y protección a los pasajeros. Comenzando por un exoesqueleto casi impenetrable, todos los componentes están diseñados para proporcionar resistencia y solidez superiores, desde el revestimiento de la estructura de acero inoxidable laminado en frío de 30x ultrarresistente, hasta el cristal blindado de Tesla.</p>
-                    <a href="#" class="post-link">Read More...</a>
-                </div>
-            </article>
-            <article class="post">
-                <div class="post-header">
-                    <div class="post-img-2"></div>
-                </div>
-                <div class="post-body">
-                    <span></span>
-                    <h2>Bugatti Chyron 300</h2>
-                    <p class="descripcion">El Bugatti Chiron es un hiperdeportivo de Bugatti, que reemplazó al Bugatti Veyron en 2016. El Bugatti Chiron se caracteriza por ser el vehículo más rápido del mundo, siendo capaz de sobrepasar los 490 kilómetros por hora.</p>
-                    <a href="#" class="post-link">Read More...</a>
-                </div>
-            </article>
-            <article class="post">
-                <div class="post-header">
-                    <div class="post-img-3"></div>
-                </div>
-                <div class="post-body">
-                    <span></span>
-                    <h2>Ford Mustang Mach-e</h2>
-                    <p class="descripcion">Tras varios meses de expectación después de que se publicase una cifra errónea y mucho más baja de lo esperado, el Ford Mustang Mach-E, el primer SUV eléctrico del fabricante norteamericano, por fin tiene precio oficial en España: se puede configurar en cuatro versiones, elegir entre dos baterías y tiene un precio de partida de 48.473 euros.</p>
-                    <a href="#" class="post-link">Read More...</a>
-                </div>
-            </article>
-        </div>
-    </section>
-
     <script src="main.js"></script>
 
 </body>

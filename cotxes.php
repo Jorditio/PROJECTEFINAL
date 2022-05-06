5520e28cd93e39818384df9efb49f0bc699698a0
Jorditio <81320747+Jorditio@users.noreply.github.com>
2022-05-06 21:23:31 +0200

    Vista de especs feta

--- cotxes.php
+++ cotxes.php
@@ -1,46 +1,57 @@
 <?php include 'connexio.php'; ?>
-<?php include 'header.php'; 
-if (isset($_COOKIE["usuari"])){
-class CRUD extends Connexio
-{
-  public function select()
+<?php include 'header.php';
+if (isset($_COOKIE["usuari"])) {
+  class CRUD extends Connexio
   {
-    $stmt = Connexio::connectar()->prepare("SELECT idindex, marca, model, any, fotos, transmissio, carburant, descripcio, usuaris_idusuaris, time	, idusuaris, username from pujades, usuaris where pujades.usuaris_idusuaris = usuaris.idusuaris order by time desc");
-    $stmt->execute();
-    return $stmt->fetchAll();
+    public function select()
+    {
+      $stmt = Connexio::connectar()->prepare("SELECT idindex, marca, model, any, fotos, transmissio, carburant, descripcio, usuaris_idusuaris, time	, idusuaris, username from pujades, usuaris where pujades.usuaris_idusuaris = usuaris.idusuaris order by time desc");
+      $stmt->execute();
+      return $stmt->fetchAll();
+    }
   }
-}
 
-$cmd = new CRUD();
-$fotos = $cmd->select();
-echo '<section class="post-list">
+  $cmd = new CRUD();
+  $fotos = $cmd->select();
+  echo '<section class="post-list">
 <div class="content">';
-foreach ($fotos as $fo) {
-  echo '<article class="post">
+  foreach ($fotos as $fo) {
+    echo '<article class="post">
       <div class="post-header">
           <img class="post-img-1" src="' . $fo["fotos"] . '">
           <div class="overlay">
             <div class="text">
-            <p class="Marca">'.$fo["marca"].' </p>
-            <p class="Model">'.$fo["model"].' </p>
-            <p class="Transmissio">'.$fo["transmissio"].' </p>
-            <p class="Carburant">'.$fo["carburant"].' </p>
+            <table>
+              <tr>
+                <th>Marca</th>
+                <th>Model</th>
+                <th>Transmissio</th>
+                <th>Carburant</th>
+              </tr>
+              <tr>
+                <td><p class="Marca">' . $fo["marca"] . ' </p></td>
+                <td><p class="Model">' . $fo["model"] . ' </p></td>
+                <td><p class="Transmissio">' . $fo["transmissio"] . ' </p></td>
+                <td><p class="Carburant">' . $fo["carburant"] . ' </p></td>
+              </tr>
+            </table>
             </div>
           </div>
       </div>
       <div class="post-body">
           <span></span>
-          <h2>'.$fo["username"].'</h2>
-          <p class="descripcion">'.$fo["descripcio"].' </p>
-          <div class="datas">'. $fo["time"].'</div>
+          <h2>' . $fo["username"] . '</h2>
+          <p class="descripcion">' . $fo["descripcio"] . ' </p>
+          <div class="datas">' . $fo["time"] . '</div>
       </div>
   </article>';
-}
-echo '</div>
-</section>';         
+  }
+  echo '</div>
+</section>';
 } else {
   header('Location: login.php');
-}?>
+} ?>
 <script src="cotxes.js"></script>
 </body>
+
 </html>

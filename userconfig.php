5c321ffd9cab47c72370af599fbb752601546bc8
Jorditio <81320747+Jorditio@users.noreply.github.com>
2022-05-17 21:08:46 +0200

    ideasa

--- userconfig.php
+++ userconfig.php
@@ -2,9 +2,17 @@
 if (isset($_COOKIE["usuari"])) {
     class CRUD extends Connexio
     {
-      public function selectConfigUser($user)
+      public function selectImgUser($user)
       {
         $stmt = Connexio::connectar()->prepare("SELECT idindex, UPPER(marca), UPPER(model), any, fotos, UPPER(transmissio), UPPER(carburant), descripcio, usuaris_idusuaris, time, idusuaris, UPPER(username) from pujades, usuaris where pujades.usuaris_idusuaris = usuaris.idusuaris and username = :username order by time desc");
+        $stmt->bindParam(":username", $user, PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll();
+      }
+
+      public function selectconfigUser($user)
+      {
+        $stmt = Connexio::connectar()->prepare("SELECT idusuaris, username, descripciouser, pff FROM usuaris WHERE username = username = :username");
         $stmt->bindParam(":username", $user, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll();
@@ -13,46 +21,16 @@ if (isset($_COOKIE["usuari"])) {
     $username = $_COOKIE["usuari"];
     $cmd = new CRUD();
 
-    
-    $fotos = $cmd->selectConfigUser($username);
-  echo '<section class="post-list">
-<div class="content">';
-  foreach ($fotos as $fo) {
-    echo '<article class="post">
-      <div class="post-header">
-          <img class="post-img-1" src="' . $fo["fotos"] . '">
-          <div class="overlay">
-            <div class="text">
-            <table>
-              <tr>
-                <th>Marca</th>
-                <th>Model</th>
-                <th>Transmissio</th>
-                <th>Carburant</th>
-              </tr>
-              <tr>
-                <td><p class="Marca">' . $fo["UPPER(marca)"] . ' </p></td>
-                <td><p class="Model">' . $fo["UPPER(model)"] . ' </p></td>
-                <td><p class="Transmissio">' . $fo["UPPER(transmissio)"] . ' </p></td>
-                <td><p class="Carburant">' . $fo["UPPER(carburant)"] . ' </p></td>
-              </tr>
-            </table>
-            </div>
-          </div>
-      </div>
-      <div class="post-body">
-          <span></span>
-          <h2>' . $fo["UPPER(username)"] . '</h2>
-          <p class="descripcion">' . $fo["descripcio"] . ' </p>
-          <div class="datas">' . $fo["time"] . '</div>
-      </div>
-  </article>';
-  }
-  echo '</div>
-</section>';
+    $configuser = $cmd->selectconfigUser($username);
+
+    echo '<form><div class="userconfig">
+    <input type="file" name="pffuser" placeholder="Edita"><br>
+    <label class="usernameconfig">'.$username.'</label>
+    </div></form>';
   } else {
     header('Location: login.php');
   }
 ?>
+
 </body>
 </html>

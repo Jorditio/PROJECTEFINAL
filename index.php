b8da63d2381938339785ef787aae93b78b57d751
ErnestDam <ernestdamjutglar@gmail.com>
2022-05-18 18:42:50 +0200

    18/05/22
    d

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <?php include 'header.php'; ?>
-
-<!-- <head>
-    <link rel="stylesheet" href="/styles/index.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-    <title>Index</title>
-</head>
-<body>
-
-
-<div class="intro">
-    <h1 class="logo-header">
-       <span class="logo">Lo</span><span class="logo">go.</span>
-    </h1>
-</div>
-
-
-<header>
-    <h4>Logo.</h4>
-</header>
-
-<script src="/styles/app.js"></script> -->
-<div>
-    <h1> som en Jordi Tio, Ernest Dam, Martí Farràs. Alumnes de 2 del CFGS de Administració de Sistemes Informatics en Xarxa de la Salle Manlleu.
-        I aquest es el nostre projecte.</h1>
-</div>
-
+<section class="py-5 my-5">
+		<div class="container">
+			<h1 class="mb-5">Manifold</h1>
+			<div class="bg-white shadow rounded-lg d-block d-sm-flex">
+				<div class="profile-tab-nav border-right">
+					<div class="p-4">
+		        <div class="img-round text-center mb-3">
+							<img src="pujades/manifold.png" alt="not found" class="shadow">
+						</div>
+					</div>
+				</div>
+				<div class="tab-content p-4 p-md-5" id="v-pills-tabContent">
+					<div class="tab-pane fade show active" id="account" role="tabpanel" aria-labelledby="account-tab">
+						<h3 class="mb-4"></h3>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+								  	<label>First Nameanfdhaufhdafoudhaoufhadouhfoufdasou`fhdoùhdsacouhfsdoahvadsòuhfoiaffç
+                                          dkdfkdjkdjkd
+                                          dkdfkdjkdjkddkkd
+                                          dkdfkdjkdjkd
+                                      </label>
+								</div>
+							</div>
+							
+						</div>
+						<div>
+						</div>
+					</div>
+					
+         
+	</section>
 
 
 </body>
